486ba277434e31946b6a756cb89f12b878d78d63
qualitytrash <44712981+qualitytrash@users.noreply.github.com>
2020-01-05 12:14:24 -0500

    Add files via upload

--- Calculating-Pi.jl
+++ Calculating-Pi.jl
@@ -1,9 +1,9 @@
 function mypi(n)
-pi = 0
-for i = 1:n
-	x = 1/(i^2)
-	pi = pi + x
-	end
-pi = sqrt(pi * 6)
-return pi
+    pi = 0
+    for i = 1:n
+        x = 1/(i^2)
+        pi += x
+    end
+    pi = sqrt(pi*6)
+    return pi
 end
