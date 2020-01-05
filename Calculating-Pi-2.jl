62137e0037096e3871134f1c0959caf029ed7146
qualitytrash <44712981+qualitytrash@users.noreply.github.com>
2020-01-05 12:36:07 -0500

    Update Calculating-Pi-2.jl

--- Calculating-Pi-2.jl
+++ Calculating-Pi-2.jl
@@ -1,5 +1,6 @@
 function mypi2(n)
     pi = 0
-    pi = sqrt(sum((1/x^2)*6 for x=1:n))
+    # Generator syntax calculates sum of pi^2/6, then sqrt(sum(...)*6) converts to pi.
+    pi = sqrt(sum((1/x^2)*6 for x=1:n)) 
     return pi
 end
