8f212ba31a26015bd4dd8e26604393ebc5dc3b35
qualitytrash <44712981+qualitytrash@users.noreply.github.com>
2020-01-05 12:34:30 -0500

    Update Calculating-Pi.jl

--- Calculating-Pi.jl
+++ Calculating-Pi.jl
@@ -1,9 +1,9 @@
 function mypi(n)
     pi = 0
-    for i = 1:n
+    for i = 1:n   # For loop calculates sum of pi^2/6. 
         x = 1/(i^2)
         pi += x
     end
-    pi = sqrt(pi*6)
+    pi = sqrt(pi*6)   # Converts the sum from the for loop to equal the value of pi.
     return pi
 end
